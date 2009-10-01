2655a21ee3238dda6f2f3ebeddee9aa3cdd96d23
Michael <okeefm@rpi.edu>
2009-09-30 20:19:12 -0400

    Commented lab2.c, the enhanced version

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,6 +1,6 @@
-/* Names: Michael O'Keefe, Virginia Hickox
-9-21-09
-LITEC Memory game: Lab 2 */
+/* Name: Michael O'Keefe
+Date: 9-30-09 (finished)
+LITEC Memory game: Lab 2, the enhanced version */
 
 #include <c8051_SDCC.h> // include files. This file is available online
 #include <stdio.h>
@@ -13,13 +13,13 @@ void Interrupt_Init(void); 	//Initialize interrupts
 void Timer0_ISR(void) interrupt 1; //ISR for Timer 0
 void ADC_Init(void); 		//Initialize ADC
 unsigned char read_AD_input(unsigned char); //Reads ADC
-void portSet(bit, bit, bit, bit, bit, bit);
-void setTimes(void);
-unsigned char random(void);
-void lightLEDS(unsigned char);
-void wait(unsigned int);
-unsigned char readButtons(void);
-void checkPB3(void);
+void portSet(bit, bit, bit, bit, bit, bit); //sets output device states
+void setTimes(void); //uses the ADC value to set the times for the LEDs on/off
+unsigned char random(void); //gets a pseudo-random number between 0 and 2
+void lightLEDS(unsigned char); //sets the LEDs based on the random number assigned to that step
+void wait(unsigned int); //uses Timer 0 to wait for the specified number of milliseconds
+unsigned char readButtons(void); //reads the buttons, and outputs a number that denotes the button pressed
+void checkPB3(void); //checks to see if pushbutton 3 is pressed, used at the end of the game
 
 
 //Declare Global Variables
@@ -64,72 +64,72 @@ void main(void)
 	
 	while(1)
 	{
-		portSet(0,1,0,0,0,0);
-		incorrect = 0;
-		if(!PB3 || (continueGame))
+		portSet(0,1,0,0,0,0); //only the red LED should be on
+		incorrect = 0; //clear the incorrect bit
+		if(!PB3 || (continueGame)) //if either the pushbutton 3 is pressed (new game) or continueGame is set (continue)
 		{
-			if (!continueGame)
+			if (!continueGame) //if it's a new game
 			{
 				printf("\r\nNew Game\r\n");
 			}
-			setTimes();
+			setTimes(); //read the ADC input, and set the LED on times and off times (in milliseconds)
 			for (i = 0; i < numLights; i++)
 			{
-				randNums[i] = random();
+				randNums[i] = random(); //get random numbers for the randNums array
 			}
-			portSet(0,0,0,0,0,0);
+			portSet(0,0,0,0,0,0); //all LEDs off
 			
-			wait(1000);
-			for (i = 0; i < numLights; i++)
+			wait(1000); //wait a second
+			for (i = 0; i < numLights; i++) //lights the LEDs for step i
 			{
 				lightLEDS(randNums[i]);
 				wait(onTime);
-				printf("Debug mode: led for step %d is %d\r\n", i+1, randNums[i]+1);
+				//printf("Debug mode: led for step %d is %d\r\n", i+1, randNums[i]+1); *this is here for debugging/testing purposes*
 				portSet(0,0,0,0,0,0);
 				wait(offTime);
 			}
 			
 			portSet(1,0,0,0,0,0);
-			for (i = 0; i < numLights; i++)
+			for (i = 0; i < numLights; i++) //read the button presses
 			{
 				buttonPress = readButtons();
-				while (buttonPress == (255))
+				while (buttonPress == (255)) //set buttonPress equal to the pressed button
 				{
 					buttonPress = readButtons();
 				}
-				while(!PB0 || !PB1 || !PB2)
+				while(!PB0 || !PB1 || !PB2) 
 				{
 				}
-				printf("You pressed button # %d\r\n", buttonPress+1);
-				if (randNums[i] != buttonPress)
+				printf("You pressed button # %d for step %d\r\n", buttonPress+1, i+1); //print out the button they pressed
+				if (randNums[i] != buttonPress) //if they made a mistake
 				{
 					printf("You pressed the incorrect button for step %d. The correct button should have been %d\r\n", i+1, randNums[i]+1);
-					portSet(1,0,1,0,0,0);
+					portSet(1,0,1,0,0,0); //sound the buzzer
 					wait(1500);
 					portSet(0,0,0,0,0,0);
-					incorrect = 1;
-					numLights = 3;
+					incorrect = 1; //set the incorrect bit
+					numLights = 3; //reset the number of lights in the sequence
 					continueGame = 0;
 					break;
 				}
-				wait(200);
+				wait(200); //debouncing the switches, just in case
 			}
-			if(!incorrect)
+			if(!incorrect) //if you get all of them right
 			{
 				printf("Congratulations! You won the level!\r\n");
-				for (i = 0; i < 3; i++)
+				for (i = 0; i < 3; i++) //flash the lights 3 times
 				{
 					portSet(1,0,0,1,1,1);
 					wait(200);
 					portSet(0,0,0,0,0,0);
 					wait(200);
 				}
-				numLights++;
-				if (numLights > 15)
+				numLights++; //add a light to the sequence (up the difficulty
+				if (numLights > 15) //if you've reached the end
 				{
-					printf("Congratulations! You're a memory master!\r\n");
+					printf("Congratulations! You're a memory master!\r\n"); 
 					numLights = 3;
-					for (i = 0; i < 10; i++)
+					for (i = 0; i < 10; i++) //flash the lights
 					{
 						portSet(1,0,0,1,1,1);
 						wait(200);
@@ -138,20 +138,20 @@ void main(void)
 					}
 				}
 				printf("If you would like to keep playing, press the start game pushbutton within 5 seconds\r\n");
-				for (i = 0; i < 5; i++)
+				for (i = 0; i < 5; i++) //print out the number of seconds remaining
 				{
 					printf("%d\r\n", 5-i);
-					checkPB3();
+					checkPB3(); ///check to see if they want to continue
 					if (continueGame)
 					{
 						printf("You are now memorizing a sequence of %d LEDs\r\n", numLights);
 						break;
 					}
-					continueGame = 0;
+					continueGame = 0; //reset the continueGame bit
 				}
 				if (!continueGame)
 				{
-					printf("Quitting.\r\n\r\n");
+					printf("Quitting.\r\n\r\n"); //if they don't want to keep going, quit
 					numLights = 3;
 				}
 			}
@@ -159,7 +159,7 @@ void main(void)
 	}
 }
 
-void checkPB3()
+void checkPB3() //check PB3 every 10 milliseconds, and if it's pressed exit
 {
 	continueGame = 0;
 	for (j = 0; j < 100; j++)
@@ -167,12 +167,13 @@ void checkPB3()
 		if(!PB3)
 		{
 			continueGame = 1;
+			break;
 		}
 		wait(10);
 	}
 	
 }
-unsigned char readButtons()
+unsigned char readButtons() //return the number of the button pressed. indexed at zero
 {
 	while(PB0 && PB1 && PB2)
 	{
@@ -187,7 +188,7 @@ unsigned char readButtons()
 		return -1;
 }
 
-void wait(unsigned int waitTime) //note: wait time is in milliseconds
+void wait(unsigned int waitTime) // wait the specified amount of time. Note: wait time is in milliseconds
 {
 		TR0 = 1;
 		while((Count / .337) < (waitTime))
@@ -199,7 +200,7 @@ void wait(unsigned int waitTime) //note: wait time is in milliseconds
 		return;
 }
 
-void lightLEDS(unsigned char n)
+void lightLEDS(unsigned char n) //light the LED indicated by n
 {
 	switch(n)
 	{
@@ -215,7 +216,7 @@ void lightLEDS(unsigned char n)
 	}
 }
 
-void ADC_Init(void)
+void ADC_Init(void) //initialize the port for the ADC
 {
     REF0CN = 0x03;                     /* Set Vref to use internal reference voltage (2.4 V) */
     ADC1CN = 0x80;                     /* Enable A/D converter (ADC1) */
@@ -223,7 +224,7 @@ void ADC_Init(void)
     ADC1CF |= 0x02;                    /* Set A/D converter gain to 1 */
 }
 
-unsigned char read_AD_input(unsigned char n)
+unsigned char read_AD_input(unsigned char n) //read from the 10k pot
 {
 	AMX1SL = n;                       /* Set P1.n as the analog input for ADC1 */
     ADC1CN = ADC1CN & ~0x20;           /* Clear the “Conversion Completed” flag */
@@ -261,6 +262,7 @@ unsigned char random(void)
 							/*value by 3 and returns the result, a value of either 0, 1 or 2*/
 }
 
+//initialize the ports as noted
 void Port_Init(void)
 {	
 	// Port 3
@@ -270,19 +272,21 @@ void Port_Init(void)
 	P2MDOUT =0xE0; //set Port 2, Pins 0-4 to open drain mode (input)
 	P2 |=0x1F;		// set Port 2, Pins 0-4 to a high impedance state (input)
 
+	//Port 1
 	P1MDIN &= ~0x01; //Set Port 1, Pin 0 to analog input
 	P1MDOUT &= ~0x01; //Set Port 1, Pin 0 to open drain mode (input)
 	P1 |= 0x01; //Set Port 1, Pin 0 to logic high
 	
 }
 
+//get the Timer 0 interrupt working
 void Interrupt_Init(void)
 {	
 	IE |= 0x02; // enable Timer0 Interrupt request 
 	EA = 1; // enable global interrupts 
 }
 
-//***************
+//Set up all the bits for the timer
 void Timer_Init(void)
 {
 	
@@ -295,7 +299,7 @@ void Timer_Init(void)
 
 }	
 
-//***************
+//Interrupt service routine for Timer 0
 void Timer0_ISR(void) interrupt 1
 {
 	TF0 = 0; // clear interrupt request 
